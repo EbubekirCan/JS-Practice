a767791ce67940ce76e7213182732b35a24c3af6
Ebubekir <ebubekirc442@gmail.com>
2023-08-14 18:18:11 +0300

    14.08.23

---
+++ day01/js/02-script.js
@@ -0,0 +1,6 @@
+//!ornek-1
+// diktortgenin alanini hesaplayan kodu yaziniz
+let uzunKenar=prompt("Lutfen uzun kenari giriniz")
+let kisaKenar=prompt("Lutfen kisa kenari giriniz")
+let alan= kisaKenar*uzunKenar
+window.alert(`Diktortgenin alani ${alan}`)
