2683624cb293b074ae54b9ab22b0013079cf9d25
Ebubekir <ebubekirc442@gmail.com>
2023-08-14 18:18:42 +0300

    14.08.23

--- day01/js/02-script.js
+++ day01/js/02-script.js
@@ -4,3 +4,17 @@ let uzunKenar=prompt("Lutfen uzun kenari giriniz")
 let kisaKenar=prompt("Lutfen kisa kenari giriniz")
 let alan= kisaKenar*uzunKenar
 window.alert(`Diktortgenin alani ${alan}`)
+
+
+let piNumber=3.14;
+console.log(typeof(piNumber))
+
+let str="Satranc";
+console.log(typeof(str))
+
+let x=null;
+let y=undefined;
+let z="";
+console.log(Boolean(x))
+console.log(Boolean(y))
+console.log(Boolean(z))
