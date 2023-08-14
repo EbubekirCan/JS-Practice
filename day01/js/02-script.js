961f2a7231443d4c2740d612ad21d6cda31d1996
Ebubekir <ebubekirc442@gmail.com>
2023-08-14 18:38:12 +0300

    14.08.23

--- day01/js/02-script.js
+++ day01/js/02-script.js
@@ -2,26 +2,20 @@
 // diktortgenin alanini hesaplayan kodu yaziniz
 let uzunKenar=prompt("Lutfen uzun kenari giriniz")
 let kisaKenar=prompt("Lutfen kisa kenari giriniz")
-
 let alan= Number(kisaKenar)*Number(uzunKenar);
-
 window.alert(`Diktortgenin alani ${alan}`) */
-
 //!ornek-2
 let piNumber = 3.14;
 console.log(typeof piNumber);
-
 let str = "Satranc";
 console.log(typeof str);
-
 let x = null;
 let y = undefined;
 let z = "";
 console.log(Boolean(x));
 console.log(Boolean(y));
 console.log(Boolean(z));
-
-//!ornek-3
+/* //!ornek-3
 let name1 = "Ali";
 let name2 = "Oya";
 let name3 = "Can";
@@ -30,8 +24,17 @@ let name5 = "";
 let name6 = undefined;
 //and op false arar
 //or op true arar
-
 console.log(name1 && name2 && name3); //Can
 console.log(name1 &&name2&& name4 && name1) //null
 console.log(name4||name5||name2||name6)//Oya
-console.log(name4||name5||false||name6)//undefined
+console.log(name4||name5||false||name6)//undefined */
+//!ornek-4
+//== degeri kontrol etmek icin kullaniriz
+//degerle birlikte type kontrolu yapmak icin === kullaniriz
+let num1=40;
+let num2="30";
+console.log(num1=="40")//true
+console.log(num1==="40")//false
+console.log(num2!==30n)//true
+console.log(num1+num2>10)//true
+console.log(num1>num2+10)//false
