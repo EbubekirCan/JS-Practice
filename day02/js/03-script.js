fa0451c3536d34a72d78c9c377913c6c1589c601
Ebubekir <ebubekirc442@gmail.com>
2023-08-16 20:25:32 +0300

    16.08.23

--- day02/js/03-script.js
+++ day02/js/03-script.js
@@ -3,12 +3,17 @@ const renk2=document.querySelector(".white")
 const renk3=document.querySelector(".blue")
 const renk4=document.querySelector(".black")
 const renk5=document.querySelector(".green")
+
 const yanlisHal=document.querySelector("#yanlis")
+
+
 renk1.innerHTML="KIRMIZI"
 renk2.innerHTML="BEYAZ";
 renk3.innerHTML="MAVI";
 renk4.innerHTML="SIYAH";
 renk5.innerHTML="YESIL";
+
+
 let yanlisDurum=`
 <div class="row">
 <div class="red">Siyah</div>
@@ -17,5 +22,7 @@ let yanlisDurum=`
 <div class="black">Mavi</div>
 <div class="green">Beyaz</div>
 </div>
+
+
 `;
-yanlisHal.innerHTML=`<h2 style=color:red;text-align:center>Yanlis Renkler</h2> ${yanlisDurum}`;
+yanlisHal.innerHTML=`<h2 style=color:red;text-align:center>YANLIS RENKLER</h2> ${yanlisDurum}`;
