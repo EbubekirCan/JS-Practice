1552a714fa0c0120f5f00bc402a61c015cf0e98f
Ebubekir <ebubekirc442@gmail.com>
2023-08-23 17:38:36 +0300

    23.08.23

---
+++ day05/assets/js/15-app.js
@@ -0,0 +1,25 @@
+let body=document.querySelector("body")
+let colorName=document.querySelector("#color")
+let buttonEl=document.querySelector("#btn")
+
+
+const colors=[
+
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "burlyywood",
+    "purple",
+    "cyan",
+    "blueviolet",
+    "darkgoldenrod",
+    "royalblue",
+    "hotpink",
+    "firebrick",
+    "chocolate",
+    "indigo",
+    "tomato",
+    "sandybrown"
+
+]
